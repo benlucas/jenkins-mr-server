665cbdbe1068d98a973749361e9eb85d7a7c48cd
Wesley Ellis <wesley.ellis@sky.uk>
2015-12-04 10:56:59 +0000

    add post method

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 var express = require('express');
-var io = require('socket.io')(80);
+//var io = require('socket.io')(80);
 var jenkinsapi = require('jenkins-api');
 var app = express();
 var jenkins = jenkinsapi.init('http://blu01:5afd2840073f0d6211b7342fb33ee7e7@jenkins.ssdm.bskyb.com:8080/jenkins', {strictSSL: false});
+var bodyParser = require('body-parser')
 
-app.get('/', function (req, res) {
-  res.json({message: "Hello World!"});
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
 
-  jenkins.build('package-match-base', {BRANCH: 'master'}, function(err, data) {
+app.post('/', function (req, res) {
+  if (!req.body) {
+  	res.json({message: 'no package name set'});
+  } else {
+  	jenkins.build(req.body.package_name, {BRANCH: 'master'}, function(err, data) {
+  		res.json({message: 'build started for ' + req.body.package_name});
+  		jenkins.last_build_info(req.body.package_name, function(err, data) {
+		  if (err){ return console.log(err); }
+		  console.log(data);
+		});
     if (err){ return console.log(err); }
-    console.log(data)
-  });
-
+    	console.log(data);
+    });
+  }
 });
 
 // http://blu01:5afd2840073f0d6211b7342fb33ee7e7@jenkins.ssdm.bskyb.com:8080/jenkins/view/sdc-packages/job/package-match-base/buildWithParameters?BRANCH=master
