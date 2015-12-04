8a5ab24643c1917537a5c2fd21aa640a1de662c2
Benjamin Lucas <benjamin.lucas@bskyb.com>
2015-12-04 12:06:53 +0000

    added socket io

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var jenkinsapi = require('jenkins-api');
+var io = require('socket.io')(3001);
 var app = express();
 var jenkins = jenkinsapi.init('http://blu01:5afd2840073f0d6211b7342fb33ee7e7@jenkins.ssdm.bskyb.com:8080/jenkins', {strictSSL: false});
 var bodyParser = require('body-parser');
@@ -12,17 +13,28 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 var currentJobNumber = 0;
 
 
+io.on('connection', function(socket){
+  console.log("someone connected!");
+  socket.on('status_update', function(payload){
+    triggerBuild(payload.repository, payload.branch, function(){
+      socket.emit('status_finished', {status:2});
+    });
+  });
+});
+
+
+
 
 var pollBuildTillDone = function(packageName, expectedBuildNumber, callback) {
   var fetch = function(){
     jenkins.build_info(packageName, expectedBuildNumber, function(err, data) {
-      if (err){
+      if (err || data.building){
         console.log(packageName + " is in progress");
         setTimeout(fetch, 5000);
         return;
       }
-      console.log(packageName + " is done!");
 
+      callback(data);
 
     });
   };
@@ -31,27 +43,30 @@ var pollBuildTillDone = function(packageName, expectedBuildNumber, callback) {
 };
 
 
-app.post('/', function (req, res) {
+var triggerBuild = function(repo, branch, callback){
+  jenkins.build(repo, {BRANCH: branch}, function(err, data) {
+    res.json({message: 'build started for ' + repo});
 
-  if (!req.body) {
-    res.json({message: 'no package name set'});
-    return;
-  }
-
-  jenkins.build(req.body.package_name, {BRANCH: 'master'}, function(err, data) {
-    res.json({message: 'build started for ' + req.body.package_name});
-
-    jenkins.last_build_info(req.body.package_name, function(err, data) {
+    jenkins.last_build_info(repo, function(err, data) {
       if (err){ return console.log(err); }
 
-      pollBuildTillDone(req.body.package_name, data.number+1);
+      pollBuildTillDone(repo, data.number+1, callback);
 
     });
 
     // if (err){ return console.log(err); }
     // console.log(data);
   });
-});
+};
+
+// app.post('/', function (req, res) {
+
+//   if (!req.body) {
+//     res.json({message: 'no package name set'});
+//     return;
+//   }
+
+// });
 
 // http://blu01:5afd2840073f0d6211b7342fb33ee7e7@jenkins.ssdm.bskyb.com:8080/jenkins/view/sdc-packages/job/package-match-base/buildWithParameters?BRANCH=master
 
