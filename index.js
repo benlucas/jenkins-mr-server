b68b1bf20b8666a498aff57c68cb1616bdd31582
Benjamin Lucas <benjamin.lucas@bskyb.com>
2015-12-04 13:15:00 +0000

    Add response when build starts

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 var express = require('express');
 var jenkinsapi = require('jenkins-api');
-var app = express();
-var http = require('http').Server(app);
 var https = require('https');
 var fs = require('fs');
 var jenkins = jenkinsapi.init('http://blu01:5afd2840073f0d6211b7342fb33ee7e7@jenkins.ssdm.bskyb.com:8080/jenkins', {strictSSL: false});
@@ -16,6 +14,8 @@ var httpsOptions = {
 var httpsServer = https.createServer(httpsOptions);
 io = require('socket.io').listen(httpsServer);
 
+var jobs = {};
+
 
 var currentJobNumber = 0;
 
@@ -26,7 +26,7 @@ io.on('connection', function(socket){
     console.log("In Status update");
     console.log(payload);
 
-    triggerBuild(payload.repository, payload.branch, function(){
+    triggerBuild(socket, payload.repository, payload.branch, function(){
       socket.emit('status_finished', {status:2});
     });
   });
@@ -53,19 +53,22 @@ var pollBuildTillDone = function(packageName, expectedBuildNumber, callback) {
 };
 
 
-var triggerBuild = function(repo, branch, callback){
+var triggerBuild = function(socket, repo, branch, callback){
   jenkins.build(repo, {BRANCH: branch}, function(err, data) {
-    res.json({message: 'build started for ' + repo});
 
     jenkins.last_build_info(repo, function(err, data) {
       if (err){ return console.log(err); }
+
+      socket.emit("status_change", {
+        job_name: repo,
+        job_number: data.number+1,
+        status: 1
+      });
 
       pollBuildTillDone(repo, data.number+1, callback);
 
     });
 
-    // if (err){ return console.log(err); }
-    // console.log(data);
   });
 };
 
@@ -74,21 +77,3 @@ httpsServer.listen(3000, function () {
 
   console.log('Example app listening at ');
 });
-
-// app.post('/', function (req, res) {
-
-//   if (!req.body) {
-//     res.json({message: 'no package name set'});
-//     return;
-//   }
-
-// });
-
-// http://blu01:5afd2840073f0d6211b7342fb33ee7e7@jenkins.ssdm.bskyb.com:8080/jenkins/view/sdc-packages/job/package-match-base/buildWithParameters?BRANCH=master
-
-// var server = http.listen(3000, function () {
-//   var host = server.address().address;
-//   var port = server.address().port;
-
-//   console.log('Example app listening at http://%s:%s', host, port);
-// });
